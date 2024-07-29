64bdd7441e8df883038df40f3f087a0840bdd2ea
Konstantin <konstantin@quadrate.org>
2024-07-29 15:49:01 +0500

    feat: add apollo 10 testnet hub address

--- utils/const.ts
+++ utils/const.ts
@@ -2,4 +2,4 @@
 
 export const StakedTonMinter = "kQCZQSYGYeDiLrLObRDi2eAeZxYhWlSRR_aylFpRzUhBIr9d"
 export const TonlinkMinter = "kQAdQ_TgB32lybYEFWVkPgn5x_P_K2biGLy0mXUyfUZHS8DN"
-export const HubAddress = "EQC-8PSFymeoRVGFSK3yNzl2ioyl7l8lOmHI-vJzJ3Mt28Hr"
+export const HubAddress = "kQCqAYx7nKCIWv6WseUVvw9y4qy9KTjdWHo1Zf0UByGc-pHF"
